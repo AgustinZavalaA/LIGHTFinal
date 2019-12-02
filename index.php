1faba75338fb8aca639f218d22d71104f31442eb
Agustin <zavalaariasagustin@hotmail.com>
2019-12-02 08:27:05 -0600

    HOli

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Index</title>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+    <h1></h1>
+    <h1> Menu</h1>
+    <h2><i class="fa fa-coffee"></i> Admin Theme</h2>
+    <h2><i class="fa fa-heart"></i> WordPress Light (Version mas actualizada)</h2>
+    <h2><i class="fa fa-diamond"></i> WordPress Dark</h2>
+<!-- partial:index.partial.html -->
+<nav class="menu">
+   <input type="checkbox" href="#" class="menu-open" name="menu-open" id="menu-open" />
+   <label class="menu-open-button" for="menu-open">
+    <span class="lines line-1"></span>
+    <span class="lines line-2"></span>
+    <span class="lines line-3"></span>
+  </label>
+
+  <a href="http://agustinza.codes/Sistema/" class="menu-item green">  <i class="fa fa-coffee"></i> </a>
+   <a href="http://agustinza.codes/miPagina-light/" class="menu-item red"> <i class="fa fa-heart"></i> </a>
+   <a href="http://agustinza.codes/miPagina-dark/" class="menu-item lightblue"> <i class="fa fa-diamond"></i> </a>
+</nav>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
